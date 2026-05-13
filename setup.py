3424a85086f2fd89c6fa9b87eeba0dfdb1b08b3e
Your-GitHub-Username <your-email@example.com>
2026-05-14 00:27:38 +0900

    Complete cleanup: Keeping only src and README

--- setup.py
+++
@@ -1,4 +0,0 @@
-from setuptools import setup
-APP = ['rajnish_hz_meter.py']
-OPTIONS = {'argv_emulation': True, 'plist': {'CFBundleName': 'Rajnish SafeGuard AI'}}
-setup(app=APP, options={'py2app': OPTIONS}, setup_requires=['py2app'])
